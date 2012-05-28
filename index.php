076868b4e2e199a28d6da6fa364b23bcf69c3625
marionserenio <marion.serenio@gmail.com>
2012-05-29 02:33:33 +0800

    saving project files

---
+++ index.php
@@ -0,0 +1,282 @@
+<!doctype html>
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!-- Consider adding an manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8">
+
+  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
+       More info: h5bp.com/b/378 -->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile viewport optimized: j.mp/bplateviewport -->
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+
+  <!-- Place favicon.ico and apple-touch-icon.png in the root directory: mathiasbynens.be/notes/touch-icons -->
+
+  <!-- CSS: implied media=all -->
+  <!-- CSS concatenated and minified via ant build script-->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet/less" href="css/style.less">
+  <script src="js/less-1.1.5.min.js"></script>
+
+  <!-- end CSS-->
+
+  <!-- More ideas for your <head> here: h5bp.com/d/head-Tips -->
+
+  <!-- All JavaScript at the bottom, except for Modernizr / Respond.
+       Modernizr enables HTML5 elements & feature detects; Respond is a polyfill for min/max-width CSS3 Media Queries
+       For optimal performance, use a custom Modernizr build: www.modernizr.com/download/ -->
+  <script src="js/libs/modernizr-2.0.6.min.js"></script>
+</head>
+
+<body>
+
+  <div class="container">
+    <header>
+      <div id="logo">
+        <h1>INSTAGROUP</h1>
+      </div>
+
+      <div id="nav_list">
+        <ul>
+          <li>Home</li>
+          <li>Products</li>
+          <li>Services</li>
+          <li>Support</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      <div id="login">
+        <a href="#">login</a>
+      </div>
+    </header>
+    <div id="main" role="main">
+      <div id="slider">
+         <div id="quote">
+
+              <h2>Aliquam bibendum ultricies 
+                  lacinia. Nullam gravida metus 
+                  non. 
+              </h2>
+
+              <h3>
+                  Mauris sit amet tortor nunc, eget consequat neque. 
+                  In diam lectus, suscipit ac sodales non, interdum 
+                  id tellus. Etiam ipsum magna.
+              </h3>
+
+         </div>
+           
+             <div id="gallery">
+                <div id="gallery_image">
+                </div>
+             </div>
+      </div>
+
+      <div class="container-fluid">
+        <div class="sidebar">
+          <h4>Aliquam bibendum</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Donec vestibulum felis ut tellus venenatis mollis. 
+          Morbi in sollicitudin est. Phasellus ac tempus massa. 
+          Ut vestibulum mauris sit amet leo congue bibendum 
+          ultricies diam porta. Proin ac sapien 
+          non lacus tristique lobortis eget 
+          ut ipsum.
+          <br />
+          <br />
+          Aliquam bibendum ultricies lacinia.
+          Nullam gravida metus non sem congue in 
+          dignissim justo pulvinar. 
+          </p>
+          <h4>Clients</h4>
+            <ol>Lorem ipsum dolor sit amet </ol>
+            <ol>consectetur adipiscing elit</ol>
+            <ol>Praesent tempus consequat </ol>
+            <ol>et interdum. Nullam at ipsum</ol>
+            <ol>Etiam convallis pretium</ol>
+            <ol>nunc nec malesuada</ol>
+            <ol>Sed rhoncus erat quis turpis</ol>
+            <ol>ullamcorper eget fermentum</ol>
+            <ol>nulla tristique. Nullam quam</ol>
+            <ol>convallis vel laoreet vel</ol>
+            <ol>Vivamus nibh est</ol>
+            <ol>tincidunt nec venenatis tempor</ol>
+            <ol>adipiscing eu velit</ol>
+            <ol>Nulla eleifend eleifend</ol>
+            <ol>Donec massa magna</ol>
+
+        </div><!-- side bar -->
+        <div class="content">
+        <h4>Mauris sit amet tortor.</h4>
+          <div class="row">
+              <div class="span6 four">
+
+                  <div id="i_payment">
+                      <div class="i_img payment"></div>
+                      <h5>INSTAPAYMENT</h5>
+                      <P>Lorem ipsum dolor sit amet, consectetur 
+                      adipiscing elit. Phasellus rutrum, 
+                      tellus non pharetra rhoncus, 
+                      nisl lorem convallis leo, id dictum 
+                      erat lorem non orci. Ut tincidunt
+                      ante vitae nisl sollicitudin convallis. 
+                      <br><br>
+                      Duis quis neque eros. Donec eget 
+                      orci augue, ac sollicitudin lorem. 
+                      
+                      </P>
+                  </div><!-- #i_payment-->
+
+                  <div id="i_cash">
+                  <div class="i_img cash"></div>
+                  <h5>INSTACASH ATM</h5>
+                  <p> Lorem ipsum dolor sit amet, consectetur 
+                      adipiscing elit. Phasellus rutrum, 
+                      tellus non pharetra rhoncus, 
+                      nisl lorem convallis leo, id dictum 
+                      erat lorem non orci. Ut tincidunt
+                      ante vitae nisl sollicitudin convallis. 
+                      <br>
+                  </p>
+                  </div><!-- #i_cash-->
+
+                  <div id="i_prepaid">
+                  <div class="i_img prepaid"></div>
+                  <h5>ISNTAPAYMENT</h5>
+                  <P>Curabitur fringilla sapien ut libero
+                     accumsan in elementum tortor convallis.
+                     Suspendisse potenti. Vivamus porttitor, 
+                     metus a accumsan varius, dui quam rutrum 
+                     lorem, eu mattis mauris nulla at mi. 
+                     Pellentesque a urna purus. Mauris a sem 
+                     at libero porta dictum ac id lectus. 
+                     Pellentesque
+                     <br><br>
+                     In auctor commodo vulputate. Suspendisse magna leo, interdum sed hendrerit a, fermentum in magna. Sed sapien tortor, ultricies non feugiat in.
+                      
+                     </P>
+                  </div><!-- #i_payment-->
+
+                  <div id="i_commerce">
+                  <div class="i_img commerce"></div>
+                  <h5>INSTAeCOMMERCE</h5>
+                  <P>Curabitur fringilla sapien ut libero
+                     accumsan in elementum tortor convallis.
+                     Suspendisse potenti. Vivamus porttitor, 
+                     metus a accumsan varius, dui quam rutrum 
+                     lorem, eu mattis mauris nulla at mi. 
+                     Pellentesque a urna purus. Mauris a sem 
+                     at libero porta dictum ac id lectus. 
+                     Pellentesque
+                     <br><br>
+                     In auctor commodo vulputate. Suspendisse magna leo, interdum sed hendrerit a, fermentum in magna. Sed sapien tortor, ultricies non feugiat in.
+                  </P>
+                  </div><!-- #i_commerce-->  
+                  </div>
+           </div>
+
+              <div id="news">
+                  <h4>News</h4>
+
+                       <div class="post">
+                       <div class="p_img">
+                       </div>
+                       <h5>Quisque nec dui est, ultricies luctus </h5>
+                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi pretium eros in ipsum suscipit fringilla. Donec quis    felis at nibh accumsan pharetra. Sed cursus nisl eu magna congue id fermentum est auctor. Pellentesque habitant morbi.
+                       <span><a href="">[read more]</a></span>
+                       <br>
+                       <span class="metadata">December 21, 2011/ News /  <a href="#">Merchant Processing</a></span>
+                       </p>
+                       </div>
+
+                       <div class="post">
+                       <div class="p_img">
+                       </div>
+                       <h5>At nisi lorem, in suscipit orci. </h5>
+                       <p>Pellentesque faucibus enim non nunc dictum feugiat. Suspendisse et tellus diam, at aliquet elit. Phasellus vel est eu erat dapibus congue id in quam. Morbi vulputate tristique leo eget. Volutpat. Fusce eros ipsum, sodales et suscipit a, tincidunt sit amet mauris.
+                       <span><a href="">[read more]</a></span>
+                       <br>
+                       <span class="metadata">December 21, 2011/ News /  <a href="#">Merchant Processing</a></span>
+                       </p>
+                       </div>
+
+                       <div class="post">
+                       <div class="p_img">
+                       </div>
+                       <h5>Mauris vitae nulla urna. Pellentesque habitant morbi</h5>
+                       <p>tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec nec sem a quam convallis volutpat. Pellentesque in magna imperdiet velit luctus scelerisque.
+                       <span><a href="">[read more]</a></span>
+                       <br>
+                       <span class="metadata">December 21, 2011/ News /  <a href="#">Merchant Processing</a></span>
+                       </p>
+                       </div>
+
+                       <div class="post">
+                       <div class="p_img">
+                       </div>
+                       <h5>Vivamus cursus dapibus magna, nec ultricies</h5>
+                       <p>tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Donec nec sem a quam convallis volutpat. Pellentesque in magna imperdiet velit luctus scelerisque.
+                       <span><a href="">[read more]</a></span>
+                       <br>
+                       <span class="metadata">December 21, 2011/ News /  <a href="#">Merchant Processing</a></span>
+                       </p>
+                       </div>
+                       
+                       
+                       
+
+
+             </div>  
+             <div class"addmore">asdasd</div> 
+        </div><!-- content --> 
+
+    </div>
+    <footer>
+    </footer>
+  </div> <!--! end of .container -->
+
+
+  <!-- JavaScript at the bottom for fast page loading -->
+
+  <!-- Grab Google CDN's jQuery, with a protocol relative URL; fall back to local if offline -->
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="js/libs/jquery-1.6.2.min.js"><\/script>')</script>
+
+
+  <!-- scripts concatenated and minified via ant build script-->
+  <script defer src="js/plugins.js"></script>
+  <script defer src="js/script.js"></script>
+  <!-- end scripts-->
+
+	
+  <!-- Change UA-XXXXX-X to be your site's ID -->
+  <script>
+    window._gaq = [['_setAccount','UAXXXXXXXX1'],['_trackPageview'],['_trackPageLoadTime']];
+    Modernizr.load({
+      load: ('https:' == location.protocol ? '//ssl' : '//www') + '.google-analytics.com/ga.js'
+    });
+  </script>
+
+
+  <!-- Prompt IE 6 users to install Chrome Frame. Remove this if you want to support IE 6.
+       chromium.org/developers/how-tos/chrome-frame-getting-started -->
+  <!--[if lt IE 7 ]>
+    <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.3/CFInstall.min.js"></script>
+    <script>window.attachEvent('onload',function(){CFInstall.check({mode:'overlay'})})</script>
+  <![endif]-->
+  
+</body>
+</html>
